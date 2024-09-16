3f03b41a40d6fc3c614dbbdbfefb8e45b1c8add3
ChippaSravan <85842385+ChippaSravan@users.noreply.github.com>
2024-09-16 17:58:10 +0530

    Update main.tf
    remove s3 backend configuration

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket         = "s306022024"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "s306022024"
+#    key            = "terraform.tfstate"
+#    region         = "us-east-1"
+#  }
+#}
 
 provider "aws" {
   region = var.aws_region
